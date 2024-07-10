195db3a029b68663b7ef45376815f7d524dd2003
rl16432 <rluo52237@gmail.com>
2024-07-11 11:07:00 +1200

    subpix refinement + ground truth fix + refactoring

--- src/controllers/controllers/localize.py
+++ src/controllers/controllers/localize.py
@@ -59,23 +59,26 @@ class CarLocalizer(Node):
     if not os.path.exists(self.debug_dir):
       os.makedirs(self.debug_dir)
 
+    # Name of the cameras to use
     self.SELECTED_CAMERA = "color"
+    self.SELECTED_DEPTH_CAMERA = "depth"
 
     self.agent_name = self.declare_parameter('agent_name', "f1tenth").get_parameter_value().string_value
     self.camera_name = self.declare_parameter('camera_name', "d435").get_parameter_value().string_value
     self.opponent_name = self.declare_parameter('opponent_name', "opponent").get_parameter_value().string_value
+    self.debug = self.declare_parameter('debug', False).get_parameter_value().bool_value
 
     self.get_logger().info(f"Subscribing to {self.agent_name}")
 
     self.color_image_sub = self.create_subscription(
       Image, 
-      f'{self.agent_name}/d435/color/image_raw', 
+      f'{self.agent_name}/{self.camera_name}/{self.SELECTED_CAMERA}/image_raw', 
       self.image_callback, 
       10)
     
     self.color_camera_info_sub = self.create_subscription(
       CameraInfo, 
-      f'{self.agent_name}/d435/color/camera_info', 
+      f'{self.agent_name}/{self.camera_name}/{self.SELECTED_CAMERA}/camera_info', 
       self.camera_info_callback, 
       10
     )
@@ -108,13 +111,13 @@ class CarLocalizer(Node):
     self.color_image_sub = Subscriber(
       self,
       Image,
-      f'{self.agent_name}/d435/color/image_raw',
+      f'{self.agent_name}/{self.camera_name}/{self.SELECTED_CAMERA}/image_raw',
     )
 
     self.depth_image_sub = Subscriber(
       self,
       Image,
-      f'{self.agent_name}/d435/depth/image_rect_raw',
+      f'{self.agent_name}/{self.camera_name}/{self.SELECTED_DEPTH_CAMERA}/image_rect_raw',
     )
 
     self.camera_pose_sub = Subscriber(
@@ -141,7 +144,7 @@ class CarLocalizer(Node):
     self.aruco_dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
 
     # Define the side length of the ArUco marker
-    side_length = 75/720
+    side_length = 0.15 # 75/720
 
     # Define the half side length
     half_side_length = side_length / 2
@@ -154,22 +157,13 @@ class CarLocalizer(Node):
         [-half_side_length, -half_side_length, 0]
     ]], dtype=np.float32)
 
-    # self.robot_state = self.create_subscription(
-    #   String, 
-    #   'f1tenth/robot_description', 
-    #   self.robot_state_callback, 
-    #   10)
-    # self.robot_state
-      
     # Used to convert between ROS and OpenCV images
     self.bridge = CvBridge()
 
   def eval_callback(self, image: Image, depth_image: Image, camera_pose: Vector3Stamped, aruco_pose: Vector3Stamped):
-    # print(depth_image)
-
-    data = self.bridge.imgmsg_to_cv2(depth_image)
-    # print(data[290, 550:])
-
+    """
+    Synced callback function for the camera image, depth image, camera pose, and ArUco pose for evaluation
+    """
     detected_aruco_pose = self.locate_aruco(image)
     aruco_pose = np.array([aruco_pose.vector.x, aruco_pose.vector.y, aruco_pose.vector.z])
     camera_pose = np.array([camera_pose.vector.x, camera_pose.vector.y, camera_pose.vector.z])
@@ -189,58 +183,24 @@ class CarLocalizer(Node):
     # Only need the camera parameters once (assuming no change)
     self.destroy_subscription(self.color_camera_info_sub)
   
-  def get_aruco_world_pose(self, data: TFMessage):
+  def get_camera_frame_id(self):
     """
-    Get the world pose of the ArUco marker
+    Get the frame ID of the camera in order to locate the transform in the TF tree
     """
-    model_pos = [tf for tf in data.transforms if tf.child_frame_id == self.opponent_name]
-    aruco_pos = [tf for tf in data.transforms if tf.child_frame_id == f'{self.opponent_name}/aruco_link']
-
-    if len(model_pos) > 0 and len(aruco_pos) > 0:
-      opp_trans, opp_rot = model_pos[0].transform.translation, model_pos[0].transform.rotation
-      aruco_trans, aruco_rot = aruco_pos[0].transform.translation, aruco_pos[0].transform.rotation
-
-      aruco_T = np.array([aruco_trans.x, aruco_trans.y, aruco_trans.z])
-      opp_T = np.array([opp_trans.x, opp_trans.y, opp_trans.z])
-      R = get_rotation_matrix_from_quaternion(opp_rot.x, opp_rot.y, opp_rot.z, opp_rot.w)
-
-      world_pos = R.T @ aruco_T + opp_T
-      print('world aruco', world_pos)
-
-      return world_pos
-    else:
-      return None
-
-  def get_camera_world_pose(self, data: TFMessage):
+    return f'{self.agent_name}/base_link/{self.agent_name}_{self.camera_name}_{self.SELECTED_CAMERA}'
+  
+  def get_aruco_frame_id(self):
     """
-    Get the world pose of the camera
+    Get the frame ID of the ArUco marker in order to locate the transform in the TF tree
     """
-    model_pos = [tf for tf in data.transforms if tf.child_frame_id == self.agent_name]
-    camera_pos = [tf for tf in data.transforms if tf.child_frame_id == f'{self.agent_name}/base_link/{self.agent_name}_{self.camera_name}_{self.SELECTED_CAMERA}']
-
-    if len(model_pos) > 0 and len(camera_pos) > 0:
-      agent_trans, agent_rot = model_pos[0].transform.translation, model_pos[0].transform.rotation
-      camera_trans, camera_rot = camera_pos[0].transform.translation, camera_pos[0].transform.rotation
-
-      camera_T = np.array([camera_trans.x, camera_trans.y, camera_trans.z])
-      agent_T = np.array([agent_trans.x, agent_trans.y, agent_trans.z])
-      R = get_rotation_matrix_from_quaternion(agent_rot.x, agent_rot.y, agent_rot.z, agent_rot.w)
-
-      world_pos = R.T @ camera_T + agent_T
-      print('world camera', world_pos)
-      return world_pos
-    else:
-      return None
-    
-  def agent_pose_callback(self, data: TFMessage):
+    return f'{self.opponent_name}/aruco_link'
+  
+  def get_camera_world_pose(self, data: TFMessage) -> tuple[np.ndarray, TransformStamped.header]:
     """
-    Callback function for the agent's pose
+    Get the world pose of the camera from the agent vehicle's published transforms
     """
-    # print(data.transforms)
-    # print()
-    
     agent_model_pos = [tf for tf in data.transforms if tf.child_frame_id == self.agent_name]
-    camera_pos = [tf for tf in data.transforms if tf.child_frame_id == f'{self.agent_name}/base_link/{self.agent_name}_{self.camera_name}_{self.SELECTED_CAMERA}']
+    camera_pos = [tf for tf in data.transforms if tf.child_frame_id == self.get_camera_frame_id()]
 
     if len(agent_model_pos) > 0 and len(camera_pos) > 0:
       agent_trans, agent_rot = agent_model_pos[0].transform.translation, agent_model_pos[0].transform.rotation
@@ -250,21 +210,17 @@ class CarLocalizer(Node):
       agent_T = np.array([agent_trans.x, agent_trans.y, agent_trans.z])
       R = get_rotation_matrix_from_quaternion(agent_rot.x, agent_rot.y, agent_rot.z, agent_rot.w)
 
-      camera_world_pose = R.T @ camera_T + agent_T
-
-      msg = Vector3Stamped()
-      msg.header = camera_pos[0].header
-      msg.vector.x, msg.vector.y, msg.vector.z = camera_world_pose
-
-      self.camera_pose_pub.publish(msg)
+      camera_world_pose = R @ camera_T + agent_T
+      return camera_world_pose, camera_pos[0].header
+    else:
+      return None, None
   
-  def opponent_pose_callback(self, data: TFMessage):
-    # print([tf for tf in data.transforms if tf.child_frame_id == self.agent_name])
-    # print(data.transforms)
-    # print([tf for tf in data.transforms if tf.child_frame_id == f'{self.agent_name}/base_link/{self.agent_name}_{self.camera_name}_{self.SELECTED_CAMERA}'])
-    
+  def get_aruco_world_pose(self, data: TFMessage) -> tuple[np.ndarray, TransformStamped.header]:
+    """
+    Get the world pose of the ArUco marker from the opponent vehicle's published transforms
+    """
     opponent_model_pos = [tf for tf in data.transforms if tf.child_frame_id == self.opponent_name]
-    opponent_aruco_pos = [tf for tf in data.transforms if tf.child_frame_id == f'{self.opponent_name}/aruco_link']
+    opponent_aruco_pos = [tf for tf in data.transforms if tf.child_frame_id == self.get_aruco_frame_id()]
     
     if len(opponent_model_pos) > 0 and len(opponent_aruco_pos) > 0:
       opp_trans, opp_rot = opponent_model_pos[0].transform.translation, opponent_model_pos[0].transform.rotation
@@ -274,10 +230,35 @@ class CarLocalizer(Node):
       opp_T = np.array([opp_trans.x, opp_trans.y, opp_trans.z])
       R = get_rotation_matrix_from_quaternion(opp_rot.x, opp_rot.y, opp_rot.z, opp_rot.w)
 
-      aruco_world_pose = R.T @ aruco_T + opp_T
-      
+      aruco_world_pose = R @ aruco_T + opp_T
+      return aruco_world_pose, opponent_aruco_pos[0].header
+    else:
+      return None, None
+    
+  def agent_pose_callback(self, data: TFMessage):
+    """
+    Callback function for the agent's pose
+
+    This publishes the 3D world position (x,y,z) of the camera on the agent vehicle
+    """
+    camera_world_pose, header = self.get_camera_world_pose(data)
+    if camera_world_pose is not None:
       msg = Vector3Stamped()
-      msg.header = opponent_aruco_pos[0].header
+      msg.header = header
+      msg.vector.x, msg.vector.y, msg.vector.z = camera_world_pose
+
+      self.camera_pose_pub.publish(msg)
+  
+  def opponent_pose_callback(self, data: TFMessage):
+    """
+    Callback function for the opponent's pose
+
+    This publishes the 3D world position (x,y,z) of the ArUco marker on the opponent vehicle
+    """
+    aruco_world_pose, header = self.get_aruco_world_pose(data)
+    if aruco_world_pose is not None and header is not None:
+      msg = Vector3Stamped()
+      msg.header = header
       msg.vector.x, msg.vector.y, msg.vector.z = aruco_world_pose
 
       self.aruco_pose_pub.publish(msg)
@@ -286,8 +267,13 @@ class CarLocalizer(Node):
     current_frame = self.bridge.imgmsg_to_cv2(image)
     current_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2RGB)
 
+    # Add subpixel refinement to marker detector
+    detector_params = cv2.aruco.DetectorParameters_create()
+    detector_params.cornerRefinementMethod = cv2.aruco.CORNER_REFINE_SUBPIX
+
     marker_corners, marker_ids, _ = cv2.aruco.detectMarkers(
       image = current_frame,
+      parameters = detector_params,
       dictionary = self.aruco_dictionary)
     if len(marker_corners) > 0:
       # tvec contains position of marker in camera frame
@@ -306,16 +292,18 @@ class CarLocalizer(Node):
     Callback function for images from the camera
     """
     saved_counter = 0
+
     # Convert ROS Image message to OpenCV image
     current_frame = self.bridge.imgmsg_to_cv2(data)
     current_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2RGB)
-    # print(self.color_intrinsics)
+    
     marker_corners, marker_ids, _ = cv2.aruco.detectMarkers(
       image = current_frame,
       dictionary = self.aruco_dictionary)
+    
     frame_copy = np.copy(current_frame)
+
     if len(marker_corners) > 0:
-      # print(marker_corners[0])
       frame_copy = cv2.aruco.drawDetectedMarkers(frame_copy, marker_corners, marker_ids)
 
       # These two approaches should produce the same result
@@ -325,12 +313,6 @@ class CarLocalizer(Node):
       
       for i in range(len(rvecs)):
         frame_copy = cv2.aruco.drawAxis(frame_copy, self.color_intrinsics, self.dist_coeffs, rvecs[i], tvecs[i], 0.1)
-
-      # R, _ = cv2.Rodrigues(rvec)
-      # print('what', rvec, tvec, R)
-      # print(self.color_intrinsics)
-      # print(R.T @ tvec)
-      # print(np.vstack((np.hstack((R, tvec)), [0, 0, 0, 1])) @ np.array([[0, 0, 0, 1]], dtype=np.float32).T)
       
     # Save the image
     # while saved_counter < 100:

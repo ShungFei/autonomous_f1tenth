a72d68a81690059c1ed06b28ce8a27525c8756e4
rl16432 <rluo52237@gmail.com>
2024-07-16 22:07:30 +1200

    add plotting for localization evaluation

---
+++ src/perception/perception/evaluation.py
@@ -0,0 +1,233 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image, CameraInfo 
+from tf2_msgs.msg import TFMessage
+from geometry_msgs.msg import Vector3Stamped, PoseStamped
+from cv_bridge import CvBridge
+from message_filters import Subscriber, ApproximateTimeSynchronizer
+import cv2
+import os
+import numpy as np
+from math import sqrt
+import seaborn as sns
+from matplotlib import pyplot as plt
+import time
+from datetime import datetime
+
+import perception.util.ground_truth as GroundTruth
+from perception.util.conversion import get_time_from_header, get_quaternion_from_rotation_matrix
+ 
+class Evaluation(Node):
+  def __init__(self):
+    """
+    Class constructor to set up the node
+    """
+    super().__init__('car_localizer')
+
+    self.FIGURES_DIR = "perception_eval"
+    # Create a directory to save evaluation figures
+    if not os.path.exists(self.FIGURES_DIR):
+      os.makedirs(self.FIGURES_DIR)
+
+    # Name of the cameras to use
+    self.SELECTED_CAMERA = "color"
+    self.SELECTED_DEPTH_CAMERA = "depth"
+    
+    plt.style.use("seaborn-v0_8")
+
+    self.agent_name = self.declare_parameter('agent_name', "f1tenth").get_parameter_value().string_value
+    self.camera_name = self.declare_parameter('camera_name', "d435").get_parameter_value().string_value
+    self.opponent_name = self.declare_parameter('opponent_name', "opponent").get_parameter_value().string_value
+    self.debug = self.declare_parameter('debug', False).get_parameter_value().bool_value
+    self.eval_time = self.declare_parameter('eval_time', 10.0).get_parameter_value().double_value
+
+    self.get_logger().info(f"Subscribing to {self.agent_name}")
+
+    self.camera_pose_pub = self.create_publisher(
+      Vector3Stamped,
+      f'{self.agent_name}/camera_pose',
+      10
+    )
+
+    self.aruco_pose_pub = self.create_publisher(
+      Vector3Stamped,
+      f'{self.opponent_name}/aruco_pose',
+      10
+    )
+    
+    self.agent_pose_sub = self.create_subscription(
+      TFMessage,
+      f'{self.agent_name}/pose', 
+      self.agent_pose_callback, 
+      10
+    )
+
+    self.opponent_pose_sub = self.create_subscription(
+      TFMessage,
+      f'{self.opponent_name}/pose',
+      self.opponent_pose_callback,
+      10
+    )
+
+    self.opp_estimated_pose_sub = Subscriber(
+      self,
+      PoseStamped,
+      f'{self.opponent_name}/pose_estimate',
+    )
+
+    self.camera_pose_sub = Subscriber(
+      self,
+      Vector3Stamped,
+      f'{self.agent_name}/camera_pose',
+    )
+
+    self.aruco_pose_sub = Subscriber(
+      self,
+      Vector3Stamped,
+      f'{self.opponent_name}/aruco_pose',
+    )
+
+    self.eval_sub = ApproximateTimeSynchronizer(
+        [self.opp_estimated_pose_sub, self.camera_pose_sub, self.aruco_pose_sub],
+        10,
+        0.1,
+    )
+    self.eval_sub.registerCallback(self.eval_callback)
+
+    self.time_list = []
+    self.ground_truth_list = []
+    self.estimated_distance_list = []
+
+    # Used to convert between ROS and OpenCV images
+    self.bridge = CvBridge()
+
+  def eval_callback(self, estimated_pose: PoseStamped, camera_pose: Vector3Stamped, aruco_pose: Vector3Stamped):
+    """
+    Synced callback function for the camera image, depth image, camera pose, and ArUco pose for evaluation
+    """
+    aruco_pose = np.array([aruco_pose.vector.x, aruco_pose.vector.y, aruco_pose.vector.z])
+    camera_pose = np.array([camera_pose.vector.x, camera_pose.vector.y, camera_pose.vector.z])
+    curr_sim_time = get_time_from_header(estimated_pose.header)
+
+    if aruco_pose is not None and camera_pose is not None and estimated_pose is not None:
+      if self.debug == True:
+        print('aruco', aruco_pose)
+        print('camera', camera_pose)
+        print('detected', estimated_pose)
+
+      estimated_tvec = np.array([estimated_pose.pose.position.x, estimated_pose.pose.position.y, estimated_pose.pose.position.z])
+
+      # Append the data to the lists
+      self.time_list.append(curr_sim_time)
+      self.ground_truth_list.append(sqrt(np.sum((aruco_pose - camera_pose)**2)))
+      self.estimated_distance_list.append(sqrt(np.sum((estimated_tvec)**2)))
+
+      print('time', curr_sim_time)
+      print('ground truth distance:', sqrt(np.sum((aruco_pose - camera_pose)**2)))
+      print('estimated distance:', sqrt(np.sum((estimated_tvec)**2)))
+
+    if curr_sim_time >= self.eval_time:
+      curr_time = datetime.now().strftime('%y_%m_%d_%H:%M:%S')
+
+      # Create a directory to save the evaluation data
+      os.makedirs(f'{self.FIGURES_DIR}/{curr_time}', exist_ok=True)
+
+      # Save the data to a file
+      self.write_eval_data(self.time_list, self.ground_truth_list, self.estimated_distance_list, curr_time)
+
+      self.plot_distance(self.time_list, self.ground_truth_list, self.estimated_distance_list, curr_time)
+      self.plot_diff(self.time_list, self.ground_truth_list, self.estimated_distance_list, curr_time)
+
+      # Destroy all relevant subscriptions
+      self.destroy_subscription(self.opp_estimated_pose_sub.sub)
+      self.destroy_subscription(self.camera_pose_sub.sub)
+      self.destroy_subscription(self.aruco_pose_sub.sub)
+
+  def write_eval_data(self, time_list, ground_truth_list, estimated_distance_list, curr_time):
+    """
+    Write the evaluation data to a file
+    """
+    data = np.column_stack((time_list, ground_truth_list, estimated_distance_list))
+    np.savetxt(f'{self.FIGURES_DIR}/{curr_time}/data.txt', data, delimiter=',', header='Time,Ground Truth,Estimated Distance', comments='')
+
+  def read_eval_data(self, curr_time):
+    """
+    Read the evaluation data from a file
+    """
+    data = np.genfromtxt(f'{self.FIGURES_DIR}/{curr_time}/data.txt', delimiter=',', skip_header=1)
+
+    # Extract the columns into separate lists
+    time_list = data[:, 0]
+    ground_truth_list = data[:, 1]
+    estimated_distance_list = data[:, 2]
+
+    return time_list, ground_truth_list, estimated_distance_list
+  
+  def plot_diff(self, time_list, ground_truth_list, estimated_distance_list, curr_time):
+    """
+    Plot the difference between the ground truth and estimated distance
+    """
+    plt.clf()
+    sns.lineplot(x=time_list, y=np.array(ground_truth_list) - np.array(estimated_distance_list))
+
+    # Add labels and title to the plot
+    plt.xlabel('Time')
+    plt.ylabel('Difference')
+    plt.title('Difference between Ground Truth and Estimated Distance')
+
+    # Save the plot as an image
+    plt.savefig(f'{self.FIGURES_DIR}/{curr_time}/diff_plot.png')
+
+  def plot_distance(self, time_list, ground_truth_list, estimated_distance_list, curr_time):
+    """
+    Plot the ground truth and estimated distance
+    """
+    plt.clf()
+    sns.lineplot(x=time_list, y=ground_truth_list, label='Ground Truth')
+    sns.lineplot(x=time_list, y=estimated_distance_list, label='Estimated Distance')
+
+    # Add labels and title to the plot
+    plt.xlabel('Time')
+    plt.ylabel('Distance')
+    plt.title('Ground Truth vs Estimated Distance')
+
+    # Save the plot as an image
+    plt.savefig(f'{self.FIGURES_DIR}/{curr_time}/distance_plot.png')
+  
+  def agent_pose_callback(self, data: TFMessage):
+    """
+    Callback function for the agent's pose
+
+    This publishes the 3D world position (x,y,z) of the camera on the agent vehicle
+    """
+    camera_world_pose, header = GroundTruth.get_camera_world_pose(data, self.agent_name, self.camera_name, self.SELECTED_CAMERA)
+    if camera_world_pose is not None:
+      msg = Vector3Stamped()
+      msg.header = header
+      msg.vector.x, msg.vector.y, msg.vector.z = camera_world_pose
+
+      self.camera_pose_pub.publish(msg)
+  
+  def opponent_pose_callback(self, data: TFMessage):
+    """
+    Callback function for the opponent's pose
+
+    This publishes the 3D world position (x,y,z) of the ArUco marker on the opponent vehicle
+    """
+    aruco_world_pose, header = GroundTruth.get_aruco_world_pose(data, self.opponent_name)
+    if aruco_world_pose is not None and header is not None:
+      msg = Vector3Stamped()
+      msg.header = header
+      msg.vector.x, msg.vector.y, msg.vector.z = aruco_world_pose
+
+      self.aruco_pose_pub.publish(msg)
+
+def main(args=None):
+  rclpy.init(args=args)
+
+  evaluation = Evaluation()
+
+  rclpy.spin(evaluation)
+
+  evaluation.destroy_node()
+  rclpy.shutdown()
--- src/perception/perception/util/conversion.py
+++ src/perception/perception/util/conversion.py
@@ -1,4 +1,6 @@
 import numpy as np
+from std_msgs.msg import Header
+from rosgraph_msgs.msg import Clock
 
 def get_euler_from_quaternion(qx, qy, qz, qw):
   """
@@ -32,3 +34,57 @@ def get_rotation_matrix_from_quaternion(qx, qy, qz, qw):
       [2 * (qx * qz - qy * qw), 2 * (qy * qz + qx * qw), 1 - 2 * (qx**2 + qy**2)]
   ])
   return R
+
+def get_quaternion_from_rotation_matrix(R):
+    """
+    Convert a rotation matrix to a quaternion.
+        
+    Input
+        :param R: The rotation matrix
+        
+    Output
+        :return qx, qy, qz, qw: The orientation in quaternion [x,y,z,w] format
+    """
+    if R[2, 2] < 0:
+        if R[0, 0] > R[1, 1]:
+            t = 1 + R[0, 0] - R[1, 1] - R[2, 2]
+            q = [t, R[0, 1] + R[1, 0], R[2, 0] + R[0, 2], R[1, 2] - R[2, 1]]
+        else:
+            t = 1 - R[0, 0] + R[1, 1] - R[2, 2]
+            q = [R[0, 1] + R[1, 0], t, R[1, 2] + R[2, 1], R[2, 0] - R[0, 2]]
+    else:
+        if R[0, 0] < -R[1, 1]:
+            t = 1 - R[0, 0] - R[1, 1] + R[2, 2]
+            q = [R[2, 0] + R[0, 2], R[1, 2] + R[2, 1], t, R[0, 1] - R[1, 0]]
+        else:
+            t = 1 + R[0, 0] + R[1, 1] + R[2, 2]
+            q = [R[1, 2] - R[2, 1], R[2, 0] - R[0, 2], R[0, 1] - R[1, 0], t]
+    
+    q = np.array(q) / (2 * np.sqrt(t))
+    return q
+
+def get_time_from_header(header: Header):
+  """
+  Convert the time from a ROS header to seconds.
+    
+  Input
+      :param header: The ROS header
+    
+  Output
+      :return time: The time in seconds
+  """
+  time = header.stamp.sec + header.stamp.nanosec * 1e-9
+  return time
+
+def get_time_from_clock(clock: Clock):
+  """
+  Convert the time from a ROS clock to seconds.
+    
+  Input
+      :param clock: The ROS clock
+    
+  Output
+      :return time: The time in seconds
+  """
+  time = clock.clock.sec + clock.clock.nanosec * 1e-9
+  return time

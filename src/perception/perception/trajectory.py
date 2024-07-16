f195b1757ab095a5ff9dbbf37301ec91fc9fb341
rl16432 <rluo52237@gmail.com>
2024-07-16 22:04:48 +1200

    use sim clock to sync trajectory updates

--- src/perception/perception/trajectory.py
+++ src/perception/perception/trajectory.py
@@ -8,8 +8,10 @@ from rclpy.executors import MultiThreadedExecutor
 from rclpy.callback_groups import ReentrantCallbackGroup
 from rclpy.publisher import Publisher
 from rclpy.timer import Timer
+from rosgraph_msgs.msg import Clock
 
 import perception.util.ground_truth as GroundTruth
+from perception.util.conversion import get_time_from_header, get_time_from_clock
 from geometry_msgs.msg import Twist
 from controllers.controller import Controller
  
@@ -31,7 +33,7 @@ class Trajectory(Node):
       "opponent": 
         [
           {"time": 1.0, "linear": 0.5, "angular": 0.0},
-          {"time": 5.0, "linear": -0.2, "angular": 0.0}
+          {"time": 7.0, "linear": -0.2, "angular": 0.0}
         ],
     }
     # Name of the cameras to use
@@ -39,15 +41,21 @@ class Trajectory(Node):
 
     self.agent_name = self.declare_parameter('agent_name', "f1tenth").get_parameter_value().string_value
     self.camera_name = self.declare_parameter('camera_name', "d435").get_parameter_value().string_value
-    self.opponent_name = self.declare_parameter('opponent_name', "opponent").get_parameter_value().string_value
+    self.opponent_name = self.declare_parameter('opponent_name', "opponent").get_parameter_value().string_value 
+    self.eval_time = self.declare_parameter('eval_time', 10.0).get_parameter_value().double_value
 
     self.start_agent_vel_pub = False
     self.start_opp_vel_pub = False
+    self.start_agent_vel_time = None
+    self.start_opp_vel_time = None
 
     self.prev_camera_pose = None
     self.camera_pose = None
     self.prev_aruco_pose = None
     self.aruco_pose = None
+
+    self.agent_checkpoint = 0
+    self.opp_checkpoint = 0
 
     self.agent_vel_publisher = self.create_publisher(
       Twist,
@@ -68,6 +76,13 @@ class Trajectory(Node):
       10
     )
 
+    self.clock_sub = self.create_subscription(
+      Clock,
+      '/clock',
+      self.clock_callback,
+      10
+    )
+
     self.opp_pose_sub = self.create_subscription(
       TFMessage,
       f'{self.opponent_name}/pose',
@@ -75,15 +90,6 @@ class Trajectory(Node):
       10
     )
 
-    timer_period = 0.1  # seconds
-
-    # Allow the agent and opponent timers to publish velocities concurrently
-    self.callback_group = ReentrantCallbackGroup()
-
-    self.agent_timer = self.create_timer(timer_period, None, callback_group=self.callback_group)
-    self.agent_timer.callback = lambda : self.update_velocity("agent", self.agent_vel_publisher, self.agent_timer)
-    self.opp_timer = self.create_timer(timer_period, None, callback_group=self.callback_group)
-    self.opp_timer.callback = lambda : self.update_velocity("opponent", self.opp_vel_publisher, self.opp_timer)
 
   def agent_pose_callback(self, data: TFMessage):
     """
@@ -97,6 +103,7 @@ class Trajectory(Node):
     self.camera_pose = camera_world_pose
     if self.prev_camera_pose is not None and self.camera_pose is not None and (self.prev_camera_pose == self.camera_pose).all():
       self.start_agent_vel_pub = True
+      self.start_agent_vel_time = get_time_from_header(header)
       self.destroy_subscription(self.agent_pose_sub)
       
   def opp_pose_callback(self, data: TFMessage):
@@ -105,46 +112,57 @@ class Trajectory(Node):
 
     Determines if the opponent has settled before publishing velocity commands
     """
-    aruco_world_pose, _ = GroundTruth.get_aruco_world_pose(data, self.opponent_name)
+    aruco_world_pose, header = GroundTruth.get_aruco_world_pose(data, self.opponent_name)
 
     self.prev_aruco_pose = self.aruco_pose
     self.aruco_pose = aruco_world_pose
     if self.prev_aruco_pose is not None and self.aruco_pose is not None and (self.prev_aruco_pose == self.aruco_pose).all():
       self.start_opp_vel_pub = True
+      self.start_opp_vel_time = get_time_from_header(header)
       self.destroy_subscription(self.opp_pose_sub)
+  
+  def clock_callback(self, data: Clock):
+    if self.start_agent_vel_pub and self.start_opp_vel_pub and self.start_agent_vel_time is not None and self.start_opp_vel_time is not None:
+      time = get_time_from_clock(data)
 
-  def update_velocity(self, vehicle_name, publisher: Publisher, timer: Timer):
-    prev_time = 0
-    if self.start_agent_vel_pub and self.start_opp_vel_pub:
-      self.destroy_timer(timer)
+      if self.agent_checkpoint >= len(self.vels["agent"]) and self.opp_checkpoint >= len(self.vels["opponent"]):
+        self.destroy_subscription(self.clock_sub)
+        return
+    
+      if self.agent_checkpoint < len(self.vels["agent"]):
+        next_agent_timestep = self.vels["agent"][self.agent_checkpoint]
+        # The velocity is only published since the time the vehicle has settled (to prevent potential flipping)
+        if time >= self.start_agent_vel_time + next_agent_timestep["time"]:
+          self.agent_checkpoint += 1
+          self.agent_vel_publisher.publish(self.create_velocity_msg(next_agent_timestep["linear"], next_agent_timestep["angular"]))
+      if self.opp_checkpoint < len(self.vels["opponent"]):
+        next_opp_timestep = self.vels["opponent"][self.opp_checkpoint]
+        if time >= self.start_opp_vel_time + next_opp_timestep["time"]:
+          self.opp_checkpoint += 1
+          self.opp_vel_publisher.publish(self.create_velocity_msg(next_opp_timestep["linear"], next_opp_timestep["angular"]))
+      
+      # Stop the vehicles after the evaluation time has passed
+      if time >= self.eval_time:
+        self.agent_vel_publisher.publish(self.create_velocity_msg(0.0, 0.0))
+        self.opp_vel_publisher.publish(self.create_velocity_msg(0.0, 0.0))
+        self.destroy_subscription(self.clock_sub)
+        return
 
-      for step in self.vels[vehicle_name]:
-        time_diff = step["time"] - prev_time
-        
-        # Wait until the time step has passed 
-        if time_diff > 0:
-          rate = self.create_rate(1.0 / time_diff, self.get_clock())
-          rate.sleep()
-          self.destroy_rate(rate)
-        # print(vehicle_name, time_diff)
+  def create_velocity_msg(self, linear, angular):
+    """
+    Create a Twist message with the given linear and angular velocities
+    """
+    msg = Twist()
+    msg.linear.x = linear
+    msg.angular.z = angular
 
-        msg = Twist()
-        msg.linear.x = step["linear"]
-        msg.angular.z = step["angular"]
-
-        publisher.publish(msg)
-
-        prev_time = step["time"]
-
+    return msg
   
 def main(args=None):
   rclpy.init(args=args)
   
   AGENT_CAR_NAME = "f1tenth"
   OPPONENT_CAR_NAME = "opponent"
-  # rclpy.spin(trajectory)
-
-  # trajectory.destroy_node()
 
   try:
     trajectory = Trajectory()
@@ -163,11 +181,3 @@ def main(args=None):
       trajectory.destroy_node()
   finally:
     rclpy.shutdown()
-
-  # rclpy.shutdown()
-  
-  # agent_controller = Controller('ftg_policy_', AGENT_CAR_NAME, 0.1)
-  # opp_controller = Controller('ftg_policy_', OPPONENT_CAR_NAME, 0.1)
-
-  # agent_controller.set_velocity(2, 0)
-  # opp_controller.set_velocity(3, 0)
